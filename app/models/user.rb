4b96142865a4b71ce58a06e97fcc4913629b675c
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-02-05 16:27:17 +0900

    チャットルーム作成機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
   has_many :room_users
   has_many :rooms, through: :room_users
 
