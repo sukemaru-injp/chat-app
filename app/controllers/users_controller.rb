a36ac8a920f1d066efe2c2f557c2fb469bcf05b5
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-02-05 12:03:44 +0900

    ユーザー編集機能実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to '/messages/index'
+    else
+      render "users/edit/#{user.id}"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
